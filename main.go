2366b4acae30ec575c47d474927cdf0be0423854
Hugo <hugo.faye@schibsted.com>
2020-02-26 15:07:37 +0100

    gestion des cas à la marge + WIP: print de l'output

--- main.go
+++ main.go
@@ -8,15 +8,39 @@ import (
 	"strconv"
 )
 
-type Key struct {
-	Contract, Category string
-}
-
 func main() {
 	jobs := readFile("technical-test-jobs.csv")
 	professions := readFile("technical-test-professions.csv")
 	output := getOutput(jobs, professions)
-	fmt.Print(output)
+	//fmt.Print(output)
+	printOutput(output)
+}
+
+func printOutput(output map[Key]int) {
+	var keys []Key
+	for k := range output {
+		//fmt.Println(k)
+		keys = append(keys, k)
+	}
+
+	var categories []string
+
+	for i := range keys {
+		contract := keys[i].Contract
+		categories = AppendIfMissing(categories, keys[i].Category)
+		fmt.Println(contract)
+	}
+
+	printFirstRow(categories)
+}
+
+func printFirstRow(categories []string) {
+	for i := range categories {
+		fmt.Print("| ")
+		fmt.Print(categories[i])
+		fmt.Print(" ")
+	}
+	fmt.Print("|")
 }
 
 // `getOutput` gets the map keys contract by category output to print
@@ -32,10 +56,24 @@ func getOutput(jobs [][]string, professions [][]string) map[Key]int {
 		}
 
 		contract := jobs[i][1]
-		jobid, _ := strconv.Atoi(jobs[i][0])
-		category := getCategory(professions, jobid)
+
+		// if no contract for the job, we increment "Other" contract
+		if contract == "" {
+			contract = "Other"
+		}
+		professionId, err := strconv.Atoi(jobs[i][0])
+
+		//if no professionId for the job, we increment "other" category
+		if err != nil {
+			output[Key{contract, "Other"}]++
+			continue
+		}
+
+		category := getCategory(professions, professionId)
 
 		output[Key{"TOTAL", "TOTAL"}]++
+		output[Key{"TOTAL", category}]++
+		output[Key{contract, "TOTAL"}]++
 		output[Key{contract, category}]++
 	}
 
@@ -58,11 +96,10 @@ func getCategory(professions [][]string, id int) string {
 		}
 	}
 
-	//TODO: trouver comment gérer ce cas
-	return "Profession ID not found"
+	return "Other"
 }
 
-// `readFile` takes a filename and returns a two-dimensional list of spreadsheet cells.
+// `readFile` takes a filename and returns a two-dimensional list of strings
 func readFile(name string) [][]string {
 
 	f, err := os.Open(name)
@@ -78,4 +115,17 @@ func readFile(name string) [][]string {
 	}
 
 	return rows
+}
+
+func AppendIfMissing(slice []string, s string) []string {
+	for _, ele := range slice {
+		if ele == s {
+			return slice
+		}
+	}
+	return append(slice, s)
+}
+
+type Key struct {
+	Contract, Category string
 }
